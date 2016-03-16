eb522c216702e2b3845233ec260a7ee4129ea3bf
TSandfeld <thomas.sandfeld@gmail.com>
2016-03-16 22:18:39 +0100

    commit1

--- java/src/dk/danskebank/markets/trading/client/ClientSample.java
+++ java/src/dk/danskebank/markets/trading/client/ClientSample.java
@@ -1,17 +1,33 @@
-package dk.danskebank.markets.trading.client;
+package BlackBox.src.dk.danskebank.markets.trading.client;
 
 import dk.danskebank.blackbox.client.IClientContract;
 import dk.danskebank.blackbox.client.TradeAction;
 
 public class ClientSample implements IClientContract {
 
+	private double lastprice = 0;
+	private boolean bought = false;
+	private double pct;
+
 	@Override 
 	public String getStrategyName() {
-		return "Sample"; 
+		return "ThomasBOT";
 	} 
-  
+
 	@Override 
 	public TradeAction tick(double price) {
-		return TradeAction.BUY;   
+		pct = ((lastprice-price)/price)*100;
+		if(pct < -5 && bought) {
+			bought = false;
+			lastprice = price;
+			return TradeAction.SELL;
+		}
+		if(5 < pct && !bought) {
+			bought = true;
+			lastprice = price;
+			return TradeAction.BUY;
+		}
+		lastprice = price;
+		return TradeAction.DO_NOTHING;
 	}
 } 
