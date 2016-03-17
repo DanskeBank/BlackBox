f471fe6f54f98cad18f3f5165e79984466e02872
tenshi <xeniaovs@gmail.com>
2016-03-17 14:14:01 +0100

    Xenchik_trade

--- java/src/dk/danskebank/markets/trading/client/ClientSample.java
+++ java/src/dk/danskebank/markets/trading/client/ClientSample.java
@@ -5,13 +5,34 @@ import dk.danskebank.blackbox.client.TradeAction;
 
 public class ClientSample implements IClientContract {
 
+	
+	private decimal price_of_stock = 0;
+	private decimal previous = 0;
+	private decimal profit = price_of_stock - previous;
+	private boolean hasBought = false;
+	
+	
 	@Override 
 	public String getStrategyName() {
-		return "Sample"; 
+		return "Xenchik_trade"; 
 	} 
   
 	@Override 
 	public TradeAction tick(double price) {
-		return TradeAction.BUY;   
+		return TradeAction.BUY;  
+		
+		if(profit < 0){
+			
+		return TradeAction.SELL;}
+		
+		
+		else if(profit > 0){
+			
+			
+			return TradeAction.BUY;
+		}
+		
+		return TradeAction.DO_NOTHING;
+	
 	}
 } 
