958078afe98ca17d0f0561e20cebf99cc4691c2a
svejstrup <madssvejstrup2@gmail.com>
2016-04-27 17:28:12 +0200

    sells if price is bigger than median buys otherwise

--- java/src/dk/danskebank/markets/trading/client/ClientSample.java
+++ java/src/dk/danskebank/markets/trading/client/ClientSample.java
@@ -3,15 +3,30 @@ package dk.danskebank.markets.trading.client;
 import dk.danskebank.blackbox.client.IClientContract;
 import dk.danskebank.blackbox.client.TradeAction;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ClientSample implements IClientContract {
+	private ArrayList<Double> previousPrices = new ArrayList<>();
 
 	@Override 
 	public String getStrategyName() {
-		return "Sample"; 
+		return "MedianIsWin";
 	} 
   
 	@Override 
 	public TradeAction tick(double price) {
-		return TradeAction.BUY;   
+		if (previousPrices.isEmpty()) {
+			previousPrices.add(price);
+			return TradeAction.BUY;
+		}
+		if (price > previousPrices.get(previousPrices.size()/2)) {
+			previousPrices.add(price);
+			Collections.sort(previousPrices);
+			return TradeAction.SELL;
+		}
+		previousPrices.add(price);
+		Collections.sort(previousPrices);
+		return TradeAction.BUY;
 	}
 } 
