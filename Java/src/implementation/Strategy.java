f5c3a46b395c176d1902f919bc991e22703fc8e1
BC1618 <BC1618@WF4129>
2016-06-11 11:46:02 -0600

    Updated java description

--- Java/src/implementation/Strategy.java
+++ Java/src/implementation/Strategy.java
@@ -4,12 +4,17 @@ import interfaces.IStrategy;
 import interfaces.TradeAction;
 /*
  * Easy example of implements of the IStrategy :) 
- * IT IS IMPORTANT TO KEEP YOUR IMPLEMENTATION OF ISTRATEGY IN THIS PACKAGE, implementation, TOGETHRE WITH THIS CLASSNAME
+ * IT IS IMPORTANT TO KEEP YOUR IMPLEMENTATION OF ISTRATEGY IN THIS PACKAGE, implementation, TOGETHERE WITH THE CLASSNAME STRATEGY
  * OTHERWISE DATA CAN NOT BE GENERATED ONCE THE STRATEGY IS SUBMITTED.
  * You have three choices specified in the TradeAction enum. These are Buy, DoNothing and Sell. 
  * You can buy at most one equity and sell one equity. Buying several times in a row is equal to buy one time and then DoNothing. The same things goes for selling.
  * Remember to test your strategy with Main.java before submitting.
- * For submitting please submit the (WhateverYourStrategyImplementationClassNameIs).class
+ * For submitting export this java project as a jar:
+ * 1. Right click project name.
+ * 2. Export.
+ * 3. Java -> Jar file
+ * 4. Finish
+ * 5. Submit the jar file
  * Good luck! :)  
  */
 public class Strategy implements IStrategy {
